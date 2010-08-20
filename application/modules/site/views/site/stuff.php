2e17e1cda29fed1a5fd3d53d13191fd2db1a2ff6
Deepwinter <deepwinter@winterroot.net>
2010-08-20 01:58:53 -0400

    Experimental application stuff

---
+++ application/modules/site/views/site/stuff.php
@@ -0,0 +1,8 @@
+<p>stuffs Content</p><?foreach($content['stuffs'] as $item):?>
+<ul>
+<?foreach($item as $field=>$value):?>
+<li><?=$field;?>: <?=$value;?></li>
+<?endforeach;?>
+</ul>
+<?endforeach;?>
+
