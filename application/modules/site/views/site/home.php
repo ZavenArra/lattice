37646f1f96facfb44d0d74940d9a4ac5a9169d1e
Deepwinter <deepwinter@winterroot.net>
2010-08-20 19:16:12 -0700

    Frontend setup and templates

--- application/modules/site/views/site/home.php
+++ application/modules/site/views/site/home.php
@@ -6,6 +6,14 @@
 </ul>
 <?endforeach;?>
 
+<p>stuffs Content</p><?foreach($content['stuffs'] as $item):?>
+<ul>
+<?foreach($item as $field=>$value):?>
+<li><?=$field;?>: <?=$value;?></li>
+<?endforeach;?>
+</ul>
+<?endforeach;?>
+
 
 <?=$listing;?>
 
