4e570c4dcab9ee63b853a4e8484cf4abf737aeee
Thiago <thiago@madeofpeople.org>
2010-09-09 17:18:28 -0400

    modules : adminpage.php added file uploader loading back in, and added a comment re: dependencies

--- modules/mop/config/adminpage.php
+++ modules/mop/config/adminpage.php
@@ -1,10 +1,12 @@
-<?
+		<?
 	$config['resources']['css'] = array();
 	$config['resources']['js'] = array(
 //		'modules/thirdparty/mootools/mootools-core-yc.js',
 		'modules/thirdparty/mootools/mootools-1.2.4-core-nc.js',
 		'modules/thirdparty/mootools/mootools-more.js',
-//		'modules/mop/views/js/digitarald/Swiff.Uploader.js',
+		'modules/mop/views/js/digitarald/Swiff.Uploader.js',
+		
+//		trying to treat this like a dynamically loaded dependency please leave it commented out and this comment for now
 //		'modules/mop/views/js/monkeyphysics/datepicker-nc.js',
 		'modules/mop/views/js/MoPCore.js',
 		'modules/mopui/views/js/MoPUI.js',
