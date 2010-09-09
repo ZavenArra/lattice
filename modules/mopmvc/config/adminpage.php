dede3e5b8318e218c51bc08c4f68685c324f6e5d
Thiago <thiago@madeofpeople.org>
2010-09-09 17:46:16 -0400

    modules : adminpage config, changed paths to new paths

--- modules/mopmvc/config/adminpage.php
+++ modules/mopmvc/config/adminpage.php
@@ -4,12 +4,12 @@
 //		'modules/thirdparty/mootools/mootools-core-yc.js',
 		'modules/thirdparty/mootools/mootools-1.2.4-core-nc.js',
 		'modules/thirdparty/mootools/mootools-more.js',
-		'modules/mop/views/js/digitarald/Swiff.Uploader.js',
+		'modules/mop/thirdparty/digitarald/Swiff.Uploader.js',
 		
 //		trying to treat this like a dynamically loaded dependency please leave it commented out and this comment for now
 //		'modules/mop/views/js/monkeyphysics/datepicker-nc.js',
-		'modules/mop/views/js/MoPCore.js',
-		'modules/mopui/views/js/MoPUI.js',
-		'modules/mop/views/js/MoPModules.js'
+		'modules/mop/MoPCore.js',
+		'modules/MoPUI.js',
+		'modules/MoPModules.js'
 	);
 ?>
