074e36527bf17e12360e3a9b3741d074f8a62a40
Thiago <thiago@madeofpeople.org>
2010-09-09 17:58:36 -0400

    modules : adminpage tweaks to paths

--- modules/mopmvc/config/adminpage.php
+++ modules/mopmvc/config/adminpage.php
@@ -2,14 +2,15 @@
 	$config['resources']['css'] = array();
 	$config['resources']['js'] = array(
 //		'modules/thirdparty/mootools/mootools-core-yc.js',
-		'modules/thirdparty/mootools/mootools-1.2.4-core-nc.js',
-		'modules/thirdparty/mootools/mootools-more.js',
-		'modules/mop/thirdparty/digitarald/Swiff.Uploader.js',
+		'modules/mop/thirdparty/mootools/mootools-1.2.4-core-nc.js',
+		'modules/mop/thirdparty/mootools/mootools-more.js',
+		'modules/mop/thirdparty/digitarald/fancyupload/Swiff.Uploader.js',
 		
 //		trying to treat this like a dynamically loaded dependency please leave it commented out and this comment for now
 //		'modules/mop/views/js/monkeyphysics/datepicker-nc.js',
+
 		'modules/mop/MoPCore.js',
-		'modules/MoPUI.js',
-		'modules/MoPModules.js'
+		'modules/mop/MoPUI.js',
+		'modules/mop/MoPModules.js'
 	);
 ?>
