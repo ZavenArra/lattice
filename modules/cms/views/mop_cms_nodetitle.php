caadcc40e19ef74982915af33ce43ccee35f6a61
Thiago <thiago@madeofpeople.org>
2010-09-18 00:36:48 -0400

    modules : mop_cms_nodetitle.php : #85 still open

--- modules/cms/views/mop_cms_nodetitle.php
+++ modules/cms/views/mop_cms_nodetitle.php
@@ -1,7 +1,9 @@
-<div class="pageTitle sixCol">
+	<div class="pageTitle sixCol">
 
-	<div class=" <?if($editable_title):?>ui-IPE<?endif;?> fiveCol floatLeft rows-2 field-title marshal-cms"><h2 class="ipe"><?=$title;?></h2></div>
-	
+	<div class=" <?if($editable_title):?>ui-IPE<?endif;?> fiveCol floatLeft rows-1 field-title marshal-cms"><h2 class="ipe h2"><?=$title;?></h2></div>
+	<div class="ui-IPE rows-1 field-slug marshal-cms"><p class="ipe p"><?$page;?></p></div>
+		
+		
 	<?if($allow_delete):?>
 		<a class="icon deleteLink" href="#" title="Delete this page."></a>
 	<?endif;?>
