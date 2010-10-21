e13277e4c907a095db5f508887cd471483ee221b
Thiago <thiago@madeofpeople.org>
2010-10-21 16:45:39 -0400

    modules : config : adminpage (cherry picked from commit 72544080c92319188c445188d862897899bee191)

--- modules/mopmvc/config/adminpage.php
+++ modules/mopmvc/config/adminpage.php
@@ -2,11 +2,11 @@
 	$config['resources']['css'] = array();
 	$config['resources']['js'] = array(
 //		'modules/thirdparty/mootools/mootools-core-yc.js',
-//		'modules/mop/thirdparty/mootools/mootools-1.2.4-core-nc.js',
-			'modules/mop/thirdparty/mootools/mootools-core-1.3-full-compat.js',
-		'modules/mop/thirdparty/mootools/mootools-1.3-more.js',
+		'modules/mop/thirdparty/mootools/mootools-1.2.4-core-nc.js',
+		// 	'modules/mop/thirdparty/mootools/mootools-core-1.3-full-compat.js',
+		// 'modules/mop/thirdparty/mootools/mootools-1.3-more.js',
 
-		// 'modules/mop/thirdparty/mootools/mootools-more.js',
+ 		'modules/mop/thirdparty/mootools/mootools-more.js',
 		'modules/mop/thirdparty/digitarald/fancyupload/Swiff.Uploader.js',
 		
 //		trying to treat this like a dynamically loaded dependency please leave it commented out and this comment for now
