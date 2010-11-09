32c380ff419323c95cbd69e3ca1a16bf9e3a675a
Deepwinter <deepwinter@winterroot.net>
2010-11-08 18:46:58 -0800

    application: generated views

--- application/views/generated/singleImage.php
+++ application/views/generated/singleImage.php
@@ -4,3 +4,7 @@
  <img id="file" src="<?=$content['main']['file']->original->fullpath;?>" width="<?=$content['main']['file']->original->width;?>" height="<?=$content['main']['file']->original->height;?>" alt="<?=$content['main']['file']->original->filename;?>" />
 <?endif;?>
 
+<?if(is_object($content['main']['file2'])):?>
+ <img id="file2" src="<?=$content['main']['file2']->original->fullpath;?>" width="<?=$content['main']['file2']->original->width;?>" height="<?=$content['main']['file2']->original->height;?>" alt="<?=$content['main']['file2']->original->filename;?>" />
+<?endif;?>
+
