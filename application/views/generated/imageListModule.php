3e882f8c605ad9f8982e30884be13b3ec27f117f
Snacks <gwen@winterroot.net>
2010-11-29 19:41:05 -0500

    application: finally commited some things and used assume-unchanged to hide the testing settings

--- application/views/generated/imageListModule.php
+++ application/views/generated/imageListModule.php
@@ -11,8 +11,8 @@
    <img id="file" src="<?=$imageListListItem['file']->original->fullpath;?>" width="<?=$imageListListItem['file']->original->width;?>" height="<?=$imageListListItem['file']->original->height;?>" alt="<?=$imageListListItem['file']->original->filename;?>" />
   <?endif;?>
 
-  <?if(is_object($imageListListItem['file'])):?>
-   <img id="file" src="<?=$imageListListItem['file']->original->fullpath;?>" width="<?=$imageListListItem['file']->original->width;?>" height="<?=$imageListListItem['file']->original->height;?>" alt="<?=$imageListListItem['file']->original->filename;?>" />
+  <?if(is_object($imageListListItem['file2'])):?>
+   <img id="file2" src="<?=$imageListListItem['file2']->original->fullpath;?>" width="<?=$imageListListItem['file2']->original->width;?>" height="<?=$imageListListItem['file2']->original->height;?>" alt="<?=$imageListListItem['file2']->original->filename;?>" />
   <?endif;?>
 
  </li>
