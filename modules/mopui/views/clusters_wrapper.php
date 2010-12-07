75a25c50e31fabf195b4a03e886513dd4d73b8c7
Deepwinter <deepwinter@winterroot.net>
2010-12-06 17:36:47 -0800

    modules: cluster label echoing

--- modules/mopui/views/clusters_wrapper.php
+++ modules/mopui/views/clusters_wrapper.php
@@ -1,5 +1,5 @@
 <div class="module cluster classPath-mop_modules_Cluster objectId-<?=$objectId;?>">
-	<label>Cluster Label</label>
+	<label><?=$label;?></label>
 	<div class="wrapper">
 		<?=$html;?>
 	</div>
