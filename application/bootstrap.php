a9d9327b396d1c45316b13fd0f3495f3b26c06aa
Deepwinter <deepwinter@winterroot.net>
2011-04-25 13:47:01 -0700

    Bootstrap

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -126,6 +126,21 @@ Kohana::modules(array(
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
+
+Route::set('default1', '<controller>(/<action>(/<id>))')
+	->defaults(array(
+		'action'     => 'index',
+	));
+
+Route::set('slug', '<uri>')
+	->defaults(array(
+		'controller'=>'asdas',
+		'action'=>'aaa'
+	));
+
+
+
+
 Route::set('default', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
 		'controller' => 'welcome',
