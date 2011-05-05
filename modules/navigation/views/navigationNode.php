7ec5d960afb4a7419a00805bd2ee33b3c933b732
Deepwinter <deepwinter@winterroot.net>
2011-05-05 09:51:24 -0700

    Navigation: workaround for published echo problem

--- modules/navigation/views/navigationNode.php
+++ modules/navigation/views/navigationNode.php
@@ -1,7 +1,16 @@
+
+<? $keys = array_keys($content);?>
+<?// echo $keys[2];?>
+<?// echo $content[$keys[2]];?>
+<?// if($keys[2] == 'published'):?>
+<?//=$content['published'];?>
+<?//endif;?>
+<?//=$content['published'];?>
+
 <li class="node <?=$content['nodeType'];?> <?=$content['contentType'];?> clearFix" id="node_<?=$content['id'];?>">
 	<h5><?=$content['title'];?></h5>
 	<div class="methods">
-		<a class="icon togglePublish published" href="#" title="unpublish NodeTitle">publish</a>
+	<a class="icon togglePublish  <?echo $content[$keys[2]]?'published':'';?> " href="#" title="unpublish <?=$content['title'];?>">publish</a>
 		<a class="icon removeNode" title="delete">delete</a>
 	</div>
 </li>
