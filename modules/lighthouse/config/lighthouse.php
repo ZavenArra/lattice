deac5f8b52cd4438bd8b1cb07c31f0d62e97bac8
Deepwinter <deepwinter@winterroot.net>
2011-05-30 20:58:49 -0700

    Cleaned up config example

--- modules/lighthouse/config/lighthouse.php
+++ modules/lighthouse/config/lighthouse.php
@@ -1,9 +1,9 @@
 <?
 // Account and user settings
-$config['account'] = "ntvb";
-$config['project_id'] = "45359";
-$config['user'] = "deepwinter2@winterroot.net";
-$config['password'] = "customer";
-$config['token'] = "316ee55566981b08957be92fb10d4f80d50269f0";
+$config['account'] = "account";
+$config['project_id'] = "id";
+$config['user'] = "email";
+$config['password'] = "password";
+$config['token'] = "apitoken";
 
 
