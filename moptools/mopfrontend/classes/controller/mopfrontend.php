900503c1403c0423a7defb18763fb5555d61e634
Deepwinter <deepwinter@winterroot.net>
2011-06-06 10:15:20 -0700

    Cleanup of mopfrontend controller

--- moptools/mopfrontend/classes/controller/mopfrontend.php
+++ moptools/mopfrontend/classes/controller/mopfrontend.php
@@ -39,45 +39,45 @@ Class Controller_MopFrontend extends Controller_Layout{
 	 * Returns: nothing, renders full webpage to browser or sents html if AJAX request
 	 */
 
-    public function action_page($pageidorslug=null) {
+	public function action_page($pageidorslug=null) {
 
-        self::$slug = $pageidorslug;
+		self::$slug = $pageidorslug;
 
-        $page = ORM::Factory('page')->where('slug', '=', $pageidorslug)->find();
-        //some access control
-        $viewName = null;
-        if ($page->loaded()) {
-            if ($page->published == false || $page->activity != null) {
-                throw new Kohana_User_Exception('Page not availabled', 'The page with identifier ' . $id . ' is does not exist or is not available');
-            }
-            //look for the template, if it's not there just print out all the data raw
-            $viewName = $page->template->templatename;
-            $viewPath = $viewName;
-            if (file_exists('application/views/frontend/' . $viewName . '.php')) {
-                $viewPath = 'frontend/'.$viewPath;
-            } else if(file_exists('application/views/generated/' . $viewPath . '.php')) {
-                $viewPath = 'generated/'.$viewPath;
-            } else {
-                $viewPath = 'default';
-            }
-            $this->view = new View($viewPath);
-        } else {
-            //check for a virtual page specified in frontend.xml
-            //a virtual page will be one that does not match a template
-            $viewname = $pageidorslug;
-            $this->view = new View($viewname);
-        }
+		$page = ORM::Factory('page')->where('slug', '=', $pageidorslug)->find();
+		//some access control
+		$viewName = null;
+		if ($page->loaded()) {
+			if ($page->published == false || $page->activity != null) {
+				throw new Kohana_User_Exception('Page not availabled', 'The page with identifier ' . $id . ' is does not exist or is not available');
+			}
+			//look for the template, if it's not there just print out all the data raw
+			$viewName = $page->template->templatename;
+			$viewPath = $viewName;
+			if (file_exists('application/views/frontend/' . $viewName . '.php')) {
+				$viewPath = 'frontend/'.$viewPath;
+			} else if(file_exists('application/views/generated/' . $viewPath . '.php')) {
+				$viewPath = 'generated/'.$viewPath;
+			} else {
+				$viewPath = 'default';
+			}
+			$this->view = new View($viewPath);
+		} else {
+			//check for a virtual page specified in frontend.xml
+			//a virtual page will be one that does not match a template
+			$viewname = $pageidorslug;
+			$this->view = new View($viewname);
+		}
 
-        //call this->view load data
-        //get all the data for the page
-        $viewContent = mop::getViewContent($viewName, $pageidorslug);
-        foreach ($viewContent as $key => $value) {
-            $this->view->$key = $value;
-        }
+		//call this->view load data
+		//get all the data for the page
+		$viewContent = mop::getViewContent($viewName, $pageidorslug);
+		foreach ($viewContent as $key => $value) {
+			$this->view->$key = $value;
+		}
 
-        //possible hook for processing content	
+		//possible hook for processing content	
 
-	$this->response->body($this->view->render());
+		$this->response->body($this->view->render());
 
 	
     }
