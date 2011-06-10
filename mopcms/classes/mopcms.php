029e62abf3fce4f7bbca230110eda4e0bebf7c5e
Thiago de Mello Bueno <thiago@nomad.local>
2011-06-10 10:04:12 -0700

    temporary bugfix in mopcms/classes/mopcms.php due to removal of staging and Creek cafes poor internet connection

--- mopcms/classes/mopcms.php
+++ mopcms/classes/mopcms.php
@@ -19,7 +19,7 @@ class MoPCMS {
 	public static function createSlug($title=NULL, $forPageId=NULL){
 		//create slug
 		if($title!=NULL){
-			$slug = preg_replace('/[^a-z0-9]/', '', $strtolower($title));
+			$slug = preg_replace('/[^a-z0-9]/', '', strtolower($title));
 			$slug = str_replace(' ', '-', strtolower($slug));
 			$slug = trim($slug);
 			$checkSlug = ORM::Factory('page')
