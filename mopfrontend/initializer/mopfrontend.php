619202ffcdb35d5ead1165324d84f6ad639ab742
Deepwinter <deepwinter@winterroot.net>
2011-06-11 13:41:45 -0700

    Moved frontend initialization to mopfrontend

--- mopfrontend/initializer/mopfrontend.php
+++ mopfrontend/initializer/mopfrontend.php
@@ -4,7 +4,7 @@ Class Initializer_MOPCms {
 
 	public function initialize() {
 		$problems = array();
-		if(!is_writable('application/media/')){
+			if(!is_writable('application/media/')){
 			$message = 'application/views/media/ must be writable';
 			$problems[$message]  =$message;
 		}
