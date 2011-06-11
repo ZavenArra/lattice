71aab4ac492597263d7f8c275a5abeb9b49ebcc1
Deepwinter <deepwinter@winterroot.net>
2011-06-11 13:46:52 -0700

    Fixed message for mopcms initializer

--- mopcms/classes/initializer/mopcms.php
+++ mopcms/classes/initializer/mopcms.php
@@ -5,7 +5,7 @@ Class Initializer_MOPCms {
 	public function initialize() {
 		$problems = array();
 		if(!is_writable('application/media/')){
-			$message = 'application/views/media/ must be writable';
+			$message = 'application/media/ must be writable';
 			$problems[$message]  =$message;
 		}
 		return $problems;
