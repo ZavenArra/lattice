2ceecb65c973cbf8386508b24b0a42badddc5061
Thiago de Mello Bueno <thiago@nomad.local>
2011-06-19 13:25:54 -0400

    Typo in getRemoveObjectObject url fixed

--- mopcms/views/js/list.js
+++ mopcms/views/js/list.js
@@ -1,18 +1,18 @@
 mop.modules.List = new Class({
-	Extends: mop.modules.MoPList,	
+	Extends: mop.modules.MoPList,
 	/* Section: Getters & Setters */
 	getAddObjectURL: function( item ){
 	    console.log( "getAddObjectURL", this.getObjectId(), this.element );
 	    return mop.util.getBaseURL() + "ajax/html/list/addObject/" + this.getObjectId();
 	},
+	getRemoveObjectURL: function( item ){
+	    return mop.util.getBaseURL() + "ajax/data/list/removeObject/" + item.getObjectId();
+	},
 	getSubmitSortOrderURL: function(){
 	    return mop.util.getBaseURL() + "ajax/html/list/saveSortOrder/" + this.getObjectId();
 	},
-	getRemoveObjectURL: function( item ){ 
-	    return mop.util.getBaseURL() + "ajax/data/list/removeObject/" + item.getObjectId();
-	},
 	toString: function(){
-	    return "[ object, mop.MoPObject, mop.modules.Module, mop.modules.List, mop.modules.MoPList, mop.modules.List ]";
+	    return "[ object, mop.MoPObject, mop.modules.Module, mop.modules.List, mop.modules.MoPList ]";
 	},
 	initialize: function( anElement, aMarshal, options ){
         this.parent( anElement, aMarshal, options );
