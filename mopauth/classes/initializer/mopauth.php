51182ee440e3e29e1ba56014af2eac1b422494b4
Deepwinter <deepwinter@winterroot.net>
2011-06-29 16:40:48 -0700

    Added schema to auth initializer

--- mopauth/classes/initializer/mopauth.php
+++ mopauth/classes/initializer/mopauth.php
@@ -13,6 +13,19 @@
 class Initializer_Mopauth {
    
    public function initialize() {
+
+		 try {
+			 ORM::Factory('auth');
+		 } catch (Exception $e) {
+			 if ($e->getCode() == 1146) { //code for table doesn't exist
+				 //install the initializedmodules table
+				 $sqlFile = Kohana::find_file('config', 'auth-schema.mysql', $ext = 'sql');
+				 $sql = file_get_contents($sqlFile[0]);
+				 mysql_query($sql);
+			 }
+		 }
+
+
 		 $admin = ORM::Factory('user')->where('username', '=', 'admin')->find();
 		 if($admin->loaded()){
 			 $admin->delete();
