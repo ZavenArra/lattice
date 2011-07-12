f83ef1bb881891d8d325a1c0d7ab53aaf18dcdce
Deepwinter <deepwinter@winterroot.net>
2011-07-12 13:15:38 -0700

    Cleaning up a bad merge

--- modules/lighthouse/config/lighthouse.php
+++
@@ -1,9 +0,0 @@
-<?
-// Account and user settings
-$config['account'] = "account";
-$config['project_id'] = "id";
-$config['user'] = "email";
-$config['password'] = "password";
-$config['token'] = "apitoken";
-
-
