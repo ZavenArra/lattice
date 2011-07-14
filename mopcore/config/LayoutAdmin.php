1a0f6b0d1fade146a8483a8965e05bf6417dfd9a
deepwinter <deepwinter@winterroot.net>
2011-07-13 23:52:46 -0500

    Fixed loading of resources specified at the layout level

--- mopcore/config/LayoutAdmin.php
+++ mopcore/config/LayoutAdmin.php
@@ -1,15 +1,7 @@
 <?
 	$config['resources']['css'] = array(
-		'moplib/thirdparty/960Grid/reset.css',
-		'moplib/thirdparty/960Grid/960.css'
 	);
 	$config['resources']['js'] = array(
-		'moplib/thirdparty/mootools/mootools.js',
- 		'moplib/thirdparty/mootools/mootools-more.js',
-		'moplib/digitarald/fancyupload/Swiff.Uploader.js',
-		'moplib/mopjs/MoPCore.js',
-		'moplib/mopjs/MoPUI.js',
-		'moplib/mopjs/MoPModules.js'
 	);
 	return $config;
 ?>
