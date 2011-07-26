ee57c5ffae87d8e477747e8973261b0ffe0a4005
Deepwinter <deepwinter@winterroot.net>
2011-07-26 13:43:22 -0700

    Added superuser role to auth schema

--- mopauth/config/auth-schema-mysql.sql
+++ mopauth/config/auth-schema-mysql.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS `roles` (
 
 INSERT INTO `roles` (`id`, `name`, `description`) VALUES(1, 'login', 'Login privileges, granted after account confirmation');
 INSERT INTO `roles` (`id`, `name`, `description`) VALUES(2, 'admin', 'Administrative user, has access to everything.');
+INSERT INTO `roles` (`id`, `name`, `description`) VALUES(3, 'superuser', 'Superuser Role.');
 
 CREATE TABLE IF NOT EXISTS `roles_users` (
   `user_id` int(10) UNSIGNED NOT NULL,
