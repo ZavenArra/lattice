bfa3ce5fd94788c9e95a0ae5a8774f7ed08c4579
Thiago de Mello Bueno <thiago@nomad.local>
2011-07-26 15:44:27 -0400

    fixed slug editing position, there was a bad merge somewhere

--- mopcms/views/mop_cms_nodetitle.php
+++ mopcms/views/mop_cms_nodetitle.php
@@ -1,13 +1,13 @@
 <div class="objectTitle">	
+	<div class="clearFix">
+		<div class="<?if($allowTitleEdit):?>ui-Text<?endif;?> grid_8 rows-1 field-title">
+			<h2 class="ipe h2"><?=$title;?></h2>
+		</div>
 
-	<div class="<?if($allowTitleEdit):?>ui-Text<?endif;?> grid_8 rows-1 field-title">
-		<h2 class="ipe h2"><?=$title;?></h2>
+		<?if($allowDelete):?>
+			<a class="icon deleteLink" href="#" title="Delete this object."></a>
+		<?endif;?>
 	</div>
-
-	<?if($allowDelete):?>
-		<a class="icon deleteLink" href="#" title="Delete this object."></a>
-	<?endif;?>
-
 	<?if(Kohana::config('cms.enableSlugEditing')):?>
 		<div class="ui-Text grid_4 field-slug">
 			<label>Slug</label>
