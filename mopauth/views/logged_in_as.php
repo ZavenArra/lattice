94421c215bb0a4604635b5dbe29abd9f989035e6
Deepwinter <deepwinter@winterroot.net>
2011-07-28 12:56:59 -0700

    Fixed logout link

--- mopauth/views/logged_in_as.php
+++ mopauth/views/logged_in_as.php
@@ -1,7 +1,7 @@
 <div class="loginstatus">
 	<div class="controls">
 	<?if(isset($username)):?>
-		You are logged in as: <?=$username;?> <a class="button" href="<?=Kohana::config('config.site_path');?>auth/logout/">logout</a>
+		You are logged in as: <?=$username;?> <a class="button" href="<?=url::site('auth/logout/');?>">logout</a>
 	<?endif;?>
 	</div>
 </div>
