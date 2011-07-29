c1d89f76168b44b92cda4f4ff182152f2c28a763
Deepwinter <deepwinter@winterroot.net>
2011-07-29 13:18:19 -0700

    Export/input with export directory supported automatically

--- mopfrontend/classes/controller/formmail.php
+++ mopfrontend/classes/controller/formmail.php
@@ -1,19 +1,40 @@
 <?
-Class Controller_Formmail extends Controller {
-	public function action_send(){
+Class Controller_Formmail extends Controller_Layout {
+
+	public $_actionsThatGetLayout = array('send');
+
+	public function sendEmail(){
 		try{
 			$content = null;
 			$name = $_POST['name'];
 			$email = $_POST['email'];
-			$subject =  Kohana::config('formmail.subject') . $_POST['subject'];
-			$message = $_POST['message'];			
-			$content = "name: " . $name . "\nemail: " . $email . "\n\nmessage:\n" . $message;
+			$subject =  Kohana::config('formmail.subject');
+			$content = "name: " . $name . "\nemail: " . $email . "\n\n";
+			foreach(Kohana::config('formmail.fields') as $field){
+				$content .= $field.': '.$_POST[$field]."\n\n";
+			}
+
 			mail( Kohana::config('formmail.email'), $subject, $content, "From: ".Kohana::config('formmail.fromLine')."\nReply-To: $email\n"); 
-			$result = array( 'success'=>true, 'message'=>"Your message has been sent. We will be in touch as soon as possible. Thank you." );
+			$result = array( 'error'=>false, 'message'=>"Your message has been sent. We will be in touch as soon as possible. Thank you." );
 		}
 		catch( Exception $e ){
+			echo 'hi';
+			      print_r($e);
 			$result = array( 'error'=>true, 'message'=>"There was a problem submitting the form, please try again later." );			
 		}
+		return $result;
+	}
+
+	public function action_ajaxsend(){
+		$result = $this->sendEmail();
 		echo json_encode( $result );
+	}
+
+	public function action_send(){
+		$result = $this->sendEmail();
+		$view = new View('formmail/done');
+		$view->result = $result;	
+		$this->response->body($view->render());
+
 	}
 }
