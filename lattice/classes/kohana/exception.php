bcd65e9537bc3f6d3302a905b4fe9fab2e608d2a
Deepwinter <deepwinter@winterroot.net>
2011-08-03 17:24:55 -0700

    Organizing lattice module

---
+++ lattice/classes/kohana/exception.php
@@ -0,0 +1,19 @@
+<?
+/*
+ * Override for Kohana_Exception that provides a function generating a message
+ * that is better formatted for sending as an error in json, for readability in firebug 
+ * following an ajax request
+ */
+Class Kohana_Exception extends Kohana_Kohana_Exception {
+
+
+	public function getOneLineErrorReport(){
+			$message = $this->getMessage();
+			foreach( $this->getTrace() as $trace){
+				if(isset($trace['file'])){
+					$message .= " ::::: ".$trace['file'].':'.$trace['line']."\n;";
+				}
+			}
+			return $message;
+	}
+}
