f8a13cf441cd55d328c5ecaef407c5c2b7a586c9
Thiago de Mello Bueno <thiago@nomad.local>
2011-08-06 13:52:16 -0400

    ui image, added imageFadeOut to destroy in ui.FileElement

--- mopjs/MoPUI.js
+++ mopjs/MoPUI.js
@@ -1725,7 +1725,7 @@ mop.ui.FileElement = new Class({
 		this.uploader.destroy();
 		this.statusElement.destroy();
 		this.element.destroy();
-		this.uploadLink = this.baseURL = this.extensions = this.filename = this.imagePreview = this.imageFadeIn = this.imgAsset = this.ogInput = this.previewElement = this.progressBar = this.sizeLimitMin = this.statusElement = this.statusHide = this.statusMessage = this.statusShow = this.uploadButton = this.uploader = this.validationError = this.invalid = null,
+		this.uploadLink = this.baseURL = this.extensions = this.filename = this.imagePreview = this.imageFadeIn = this.imageFadeOut = this.imgAsset = this.ogInput = this.previewElement = this.progressBar = this.sizeLimitMin = this.statusElement = this.statusHide = this.statusMessage = this.statusShow = this.uploadButton = this.uploader = this.validationError = this.invalid = null,
 		this.parent();
 	}
 });
