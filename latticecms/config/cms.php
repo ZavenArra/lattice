e3665f022f5d693e7e770f9589fafd85bb9dc93c
Thiago de Mello Bueno <thiago@nomad.local>
2011-08-31 14:41:50 -0400

    added defaultLanguage to latticesettings.php, both latticecms & latticecore config's

--- latticecms/config/cms.php
+++ latticecms/config/cms.php
@@ -1,13 +1,9 @@
 <?
-$config['resources']['libraryjs'] = array(
-	'views/js/latticesettings',
-);
-
+$config['resources']['libraryjs'] = array( 'views/js/latticesettings' );
 $config['navigationRequest'] = 'navigation';
 $config['layout'] = 'LayoutAdmin';
-
 $config['enableSlugEditing'] = true;
-
 $config['graphRootNode'] = 'cmsRootNode';
+$config['defaultLanguage'] = 'en';
 
 return $config;
--- latticecore/config/lattice.php
+++ latticecore/config/lattice.php
@@ -1,14 +1,11 @@
 <?
-
+$config['siteTitle'] = "LatticeCMS - It's Made of People";
 $config['mediapath'] = 'application/media/';
 $config['stagingmediapath'] = 'staging/application/media/';
-
 $config['staging_enabled'] = false;
 $config['slugs_enabled'] = true;
 $config['loginTimeOut'] = 1800; //in seconds
-
-$config['siteTitle'] = "LatticeCMS - It's Made of People";
-
+$config['defaultLanguage'] = 'en';
 $config['live'] = false;
 
 return $config;
