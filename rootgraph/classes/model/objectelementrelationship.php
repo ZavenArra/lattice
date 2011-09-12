7058525703ef0e70bc0f7d9afe645d0491a50091
Deepwinter <deepwinter@winterroot.net>
2011-09-11 22:51:58 -0400

    Added objectrelationship model

---
+++ rootgraph/classes/model/objectelementrelationship.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of objectelements
+ *
+ * @author deepwinter1
+ */
+class Model_ObjectElementRelationship extends ORM {
+//put your code here
+}
+?>
