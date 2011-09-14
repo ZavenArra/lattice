a84898f9884c282b57b235c1bc2695d448725786
0ak <0ak@WinterRoot.(none)>
2011-09-14 13:06:10 -0400

    Fixing issue with merge, step 1.

--- latticeui/views/ui_pulldown.php
+++
@@ -1,8 +0,0 @@
-<div class="ui-Pulldown <?=$class;?>" data-field="<?=$name;?>">
-	<label class="groupLabel"><?=$label;?></label>
-	<!-- =form::dropdown($field, $options, $value, 'class="pulldown field-'.$field.'"'); 
-        ?=form::select('state', array_slice(Kohana::config('formVariables.states'), 1), $customer->state, ' style="margin-bottom:7px;width:100%"')?>
-	?=form::select($field, $options, $value, 'class="pulldown field-'.$field.'"');  ?>
-	-->
-	 <?=form::select( $groupLabel, $options );?> 
-</div>
