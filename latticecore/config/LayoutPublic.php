84ab1786e6e95ec65c5e4f5f27c0a5ce5647f327
Thiago de Mello Bueno <thiago@nomad.local>
2011-09-14 14:29:41 -0400

    changed layoutpublic config defaults

--- latticecore/config/LayoutPublic.php
+++ latticecore/config/LayoutPublic.php
@@ -1,8 +1,5 @@
 <?
-	$config['resources']['css'] = array(
-		'application/views/css/publicStyles.css',
-	);
-	$config['resources']['js'] = array(
-	);
+	$config['resources']['css'] = array();
+	$config['resources']['js'] = array();
 	return $config;
 ?>
