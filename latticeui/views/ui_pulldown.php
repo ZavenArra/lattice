74db3e8e25ffdfd9f00e759eec4bfe4e8b6ada67
Deepwinter <deepwinter@winterroot.net>
2011-09-15 23:01:05 -0700

    Working on pulldown

--- latticeui/views/ui_pulldown.php
+++ latticeui/views/ui_pulldown.php
@@ -1,4 +1,4 @@
 <div class="ui-Pulldown <?=$class;?>" data-field="<?=$name;?>">
 	<label class="groupLabel"><?=$label;?></label>
-	<?=form::dropdown($name, $options, $value, 'class="pulldown field-'.$name.'"');?>
+	<?=form::select($name, $options, $value, 'class="pulldown field-'.$name.'"');?>
 </div>
