d105df77b70c70ed8be5a7e8d4233738897a63d4
Deepwinter <deepwinter@winterroot.net>
2011-09-26 17:33:59 -0700

    Added validation messages for user model errors

---
+++ latticeauth/messages/validation/user.php
@@ -0,0 +1,12 @@
+<?
+
+$errors = array(
+	'email' => array(
+		'email_available' => 'That email is not available.',
+	),
+	'username' => array(
+		'username_available' => 'That username is not available.',
+	),
+);
+
+return $errors;
