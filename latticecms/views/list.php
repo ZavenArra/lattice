b0ff651d2381d26c556fbef7a1d444c8db4a9a55
Thiago de Mello Bueno <thiago@nomad.local>
2011-10-12 11:49:21 -0700

    view list.php changes for adding multipleobjecttypes

--- latticecms/views/list.php
+++ latticecms/views/list.php
@@ -1,15 +1,16 @@
 <div id="list_<?=$listObjectId;?>" data-objectid="<?=$listObjectId;?>" class="module <?=$class;?> classPath-lattice_modules_List">
-   <!-- changed id to list, since this is what controls submission
-   but of course this can't be the id, since there can be multiple lists in the object
-   instance idea needs to be changes -->
 	<?if(isset($label) && $label):?>
 	<label class='listLabel'><?=$label;?></label>
 	<?endif;?>
-	<?foreach($addableObjects as $addableObject):?>
-		<div class="controls clearFix"><a href="addObject/<?=$listObjectId;?>/<?=$addableObject['objectTypeId'];?>" class="addItem button"><?=$addableObject['objectTypeAddText'];?></a></div>	
-	<?endforeach;?>
-		<ul class="listing"><?=$items;?></ul>
-	<?foreach($addableObjects as $addableObject):?>
-		<div class="controls clearFix"><a href="addObject/<?=$listObjectId;?>/<?=$addableObject['objectTypeId'];?>" class="addItem button"><?=$addableObject['objectTypeAddText'];?></a></div>	
-	<?endforeach;?>
+	<div class="controls clearFix">
+		<?foreach($addableObjects as $addableObject):?>
+		<a href="addObject/<?=$listObjectId;?>/<?=$addableObject['objectTypeId'];?>" class="addItem button"><?=$addableObject['objectTypeAddText'];?></a>
+		<?endforeach;?>
+	</div>	
+	<ul class="listing"><?=$items;?></ul>
+	<div class="controls clearFix">
+		<?foreach($addableObjects as $addableObject):?>
+		<a href="addObject/<?=$listObjectId;?>/<?=$addableObject['objectTypeId'];?>" class="addItem button"><?=$addableObject['objectTypeAddText'];?></a>	
+		<?endforeach;?>
+	</div>
 </div>
