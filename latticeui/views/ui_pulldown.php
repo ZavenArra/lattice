7dacb810fd8eddfe4ef328465df3d95313042846
Deepwinter <deepwinter@winterroot.net>
2011-12-15 02:43:44 -0500

    ui_pulldown was reindexing the options array when adding "select" text to top of array

--- latticeui/views/ui_pulldown.php
+++ latticeui/views/ui_pulldown.php
@@ -1,5 +1,11 @@
 <div class="ui-Pulldown <?=$class;?>" data-field="<?=$name;?>">
 	<label class="groupLabel"><?=$label;?></label>
-	 <? array_unshift($options, 'Select');?>
-	 <?=form::select( $label, $options, $value );?> 
+  <?
+    $optionsWithSelectText = array();
+    $optionsWithSelectText["0"] = 'Select';
+    foreach($options as $value => $key){
+      $optionsWithSelectText[$value] = $key;
+    }
+?>
+	 <?=form::select( $label, $optionsWithSelectText, $value );?> 
 </div>
