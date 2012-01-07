4d32dff67a54f61c240d0e09089ae3fb8c34f85a
deepwinter@winterroot.net <deepwinter@winterroot.net>
2012-01-07 16:56:10 -0600

    Navigation node truncates with ellipsis at 30 chars

--- navigation/views/navigationNode.php
+++ navigation/views/navigationNode.php
@@ -1,6 +1,6 @@
 <? $keys = array_keys($content);?>
 <li class="node <?=$content['nodeType'];?> <?=$content['contentType'];?> clearFix" id="node_<?=$content['id'];?>">
-	<h5><?=$content['title'];?></h5>
+  <h5><?  echo strlen($content['title']) > 30 ? substr($content['title'], 0, 30).'...' : $content['title']; ?></h5>
 	<div class="methods">
 		<?if(isset($content['allowTogglePublish']) && $content['allowTogglePublish']=='true' ):?>
 		<a class="icon togglePublishedStatus <?echo $content[$keys[2]]?'published':'';?> " href="#" title="unpublish <?=$content['title'];?>">publish</a>
