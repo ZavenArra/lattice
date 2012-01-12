b96f5217ce489729a46300a1a55834d29e342774
Deepwinter <deepwinter@winterroot.net>
2012-01-11 21:38:43 -0500

    Moved all rootgraph into lattice, prepping for single directory module

--- lattice/init.php
+++ lattice/init.php
@@ -1,3 +1,33 @@
 <?
 
 require('lattice/lattice/includes/mysqlfuncs.php');
+
+//check for setup
+Lattice_Initializer::check(
+	array(
+		'rootgraph',
+	)
+);
+
+
+Route::set('graph_addChild', '<id>/<action>/<type>', array(
+	'action' => 'addChild',
+)
+		)
+		->defaults(
+			array(
+				'controller' => 'graph',
+			));
+
+/*
+ * Default path to allow 4 arguments to graph if necessary
+ */
+Route::set('graph', 'graph/<action>(/<param1>(/<param2>))', array(
+
+	)
+)
+->defaults(
+	array(
+		'controller' => 'graph',
+	));
+
