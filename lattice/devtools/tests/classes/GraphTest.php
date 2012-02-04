08807038554db83ceb35611fa6db7bf54ce72937
Deepwinter <deepwinter@winterroot.net>
2012-02-03 18:04:22 -0800

    Added tests for graph

--- lattice/devtools/tests/classes/GraphTest.php
+++ lattice/devtools/tests/classes/GraphTest.php
@@ -6,4 +6,29 @@ Class GraphTest extends Kohana_UnitTest_TestCase {
     $this->assertNotNull($object);
 	}
 
+	public function testGraphCreateObject(){
+    $object = Graph::object();
+    $object->save();
+    $this->assertTrue($object->loaded());
+    return $object->id;
+	}
+  
+  /**
+   * @depends testGraphCreateObject
+   */
+  public function testGraphLoadObject($objectId){
+    $object = Graph::object($objectId);
+    $this->assertTrue($object->loaded());
+  }
+
+  
+  /**
+   * @depends testGraphCreateObject
+   */
+  public function testGraphDeleteObject($objectId){
+    $object = Graph::object($objectId);
+    $object->delete();
+    $this->assertFalse($object->loaded());
+  }
+
 }
