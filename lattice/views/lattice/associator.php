9084cf8a98fc9385de1a016d322b20c0dff2ff3a
Deepwinter <deepwinter@winterroot.net>
2012-02-13 20:23:44 -0800

    Support for lambda association pool functions

--- lattice/views/lattice/associator.php
+++ lattice/views/lattice/associator.php
@@ -1,10 +1,14 @@
 <div id="" data-objectid="<?=$parentId;?>" data-lattice="<?=$lattice;?>" class="module associator classPath-lattice_modules_Associator clearFix">
 
+<?=$label;?>
+
 <ul class="associated">
 <?foreach($associated as $view):?>
   <?=$view->render();?>
 <?endforeach;?>
 </ul>
+
+<?=$poolLabel;?>
 
 <ul class="pool">
 <?foreach($pool as $view):?>
