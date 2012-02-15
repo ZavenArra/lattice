a180f4c6ecbcaf90ab6c7b0be91fda58ace8994a
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-02-14 20:11:55 -0500

    associator_radio view, item view and js added.

---
+++ lattice/views/lattice/associator_radios.php
@@ -0,0 +1,10 @@
+<div id="" data-objectid="<?=$parentId;?>" data-lattice="<?=$lattice;?>" class="module associator_radio classPath-lattice_modules_RadioAssociator clearFix">
+	<div class="ui-RadioGroup " data-field="singleRadioGroup">
+			<label class="groupLabel">Single Radio Group</label>
+			<fieldset>
+ 				<label for="singleRadioGroup30764577001329257038_1"><input type="radio" id="singleRadioGroup30764577001329257038_1" name="singleRadioGroup30764577001329257038" value="1">One</label>
+				<label for="singleRadioGroup30764577001329257038_2"><input type="radio" id="singleRadioGroup30764577001329257038_2" name="singleRadioGroup30764577001329257038" value="2">Two</label>
+				<label for="singleRadioGroup30764577001329257038_3"><input type="radio" id="singleRadioGroup30764577001329257038_3" name="singleRadioGroup30764577001329257038" value="3">Three</label>
+			</fieldset>
+	</div>
+</div>
