483a8b41d581adb860b5d99aaf6fbadbd94c8f8c
Deepwinter <deepwinter@winterroot.net>
2012-02-22 19:30:08 -0800

    Moving assets into their own repository

--- lattice/views/js/latticesettings.php
+++
@@ -1,7 +0,0 @@
-/* conditional is probably not necessary here since its latticesettings should be the first thing to load always... its vestigeous */
-if( !lattice ) var lattice = {
-	baseURL: '<?=URL::base();?>',
-	loginTimeout: <?=Kohana::config('lattice.loginTimeOut');?>,
-	defaultLanguage: '<?=Kohana::config('lattice.defaultLanguage');?>',
-	debug: true
-};
