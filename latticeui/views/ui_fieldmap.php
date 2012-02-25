670f03e39895a4001beec20e890eb9c9675e8f13
Deepwinter <deepwinter@winterroot.net>
2012-02-24 21:01:05 -0800

    Removed unused file

--- latticeui/views/ui_fieldmap.php
+++
@@ -1,8 +0,0 @@
-<?$values = unserialize($values);?>
-<?if(is_array($values)):?>
-	<?foreach($values as $key => $value):?>
-	<div class="ui-Pulldown" data-field="<?=$key;?>">
-		<?=$key;?> : <?=form::dropdown($key, $options, $value, 'class="pulldown field-'.$key.'"');?>
-		</div>
-	<?endforeach;?>
-<?endif?>
