15da485af6f33304f5bdffe47d61ae3616e95055
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-02-27 21:31:45 -0500

    some nav image size tweaks, associator drawer still in progress (should be on a feature branch, sorry)

--- lattice/views/lattice/associator.php
+++ lattice/views/lattice/associator.php
@@ -1,29 +1,32 @@
 <div id="" data-objectid="<?=$parentId;?>" data-lattice="<?=$lattice;?>" class="module associator classPath-lattice_modules_Associator clearFix">
 
-<h5><?=$label;?></h5>
-<ul class="associated clearFix">
-<?foreach($associated as $view):?>
-  <?=$view->render();?>
-<?endforeach;?>
-</ul>
+	<h4><?=$label;?></h4>
 
-<h5><?=$poolLabel;?></h5>
-
-<div class="pool clearFix">
-
-	<div class="filter">
-		<label>
-			<input type="text" name="filter" value="" id="FILTERuniqueID" />
-			<a href="#" class="button">Filter Pool</a>
-		</label>
-	</div>
-	
-	<ul class="pool clearFix">
-	<?foreach($pool as $view):?>
+	<ul class="associated clearFix">
+	<?foreach($associated as $view):?>
 	  <?=$view->render();?>
 	<?endforeach;?>
 	</ul>
 
-<div>
+	<div class="actuator clearFix">
+		<a href="#" title="<?=$poolLabel;?>" class="icon closed"><?=$poolLabel;?></a>
+	</div>
 
+	<div class="poolcontainer clearFix">
+
+		<div class="filter clearFix">
+			<label>
+				<input type="text" name="filter" value="" id="FILTERuniqueID" />
+				<a href="#" class="button">Filter Pool</a>
+			</label>
+		</div>
+	
+		<ul class="pool clearFix">
+		<?foreach($pool as $view):?>
+		  <?=$view->render();?>
+		<?endforeach;?>
+		</ul>
+
+	</div>
+	
 </div>
