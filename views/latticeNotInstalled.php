5f7f61368951287ba1aee32d789013dcb29f9adb
Deepwinter <deepwinter@winterroot.net>
2012-04-02 16:43:33 -0700

    More descriptive setup screen

--- views/latticeNotInstalled.php
+++ views/latticeNotInstalled.php
@@ -3,6 +3,6 @@
 		<h3>Lattice is Not Installed</h3>
 	</div>
 	<div class="modal container_12">
-		<a class="button" href="<?=url::base('http');?>setup">Install lattice now</a>
+  <a class="button" href="<?=url::base('http');?>setup">Install lattice now</a>
 	</div>
 </div>
