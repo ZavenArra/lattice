1ee63a9cdcaa72625aa469de5b01295b763746d4
Deepwinter <deepwinter@winterroot.net>
2012-04-12 00:56:53 -0700

    Added name field to dateRange

--- views/ui/dateRange.php
+++ views/ui/dateRange.php
@@ -2,6 +2,6 @@
 	<?if(isset($label)):?>
 		<label><?=$label;?></label>
 	<?endif;?>
-	<input type="text" value="<?=$startDate;?>-<?=$endDate;?>" />
+  <input type="text" name='<?=$name;?>" value="<?=$startDate;?>-<?=$endDate;?>" />
 	<img src="<?=url::base();?>lattice/lattice/resources/images/spinner.gif" width="12" height="12" alt="saving date range..." class="hidden spinner" />
 </div>
