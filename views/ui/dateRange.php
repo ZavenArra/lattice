dbfac838f1848918d2c7ab89b688f402185e81d8
Deepwinter <deepwinter@winterroot.net>
2012-04-12 01:04:22 -0700

    Removed debug

--- views/ui/dateRange.php
+++ views/ui/dateRange.php
@@ -2,7 +2,6 @@
 	<?if(isset($label)):?>
 		<label><?=$label;?></label>
 	<?endif;?>
-  asdfas
   <input type="text" name="<?=$name;?>" value="<?=$startDate;?>-<?=$endDate;?>" />
 	<img src="<?=url::base();?>lattice/lattice/resources/images/spinner.gif" width="12" height="12" alt="saving date range..." class="hidden spinner" />
 </div>
