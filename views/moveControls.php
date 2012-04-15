be90972d1cda97c3dd0423cd2f0aa1163502e332
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-04-15 09:04:03 -0400

    moveControls

--- views/moveControls.php
+++ views/moveControls.php
@@ -1,2 +1,7 @@
-
-<?= form::select('move', $potentialParents); ?>
+<?if( count($potentialParents) >1 ):?>
+<div class="moveWidget">
+	<h5>Move</h5>
+	<? $opts = array( 'Move this page to...' ) + $potentialParents;?>
+	<?=form::select('move', $opts ); ?>
+</div>
+<?endif;?>
