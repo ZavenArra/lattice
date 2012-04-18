3d7771c7c4e88ef110593e45b96839703d614fc4
Deepwinter <deepwinter@winterroot.net>
2012-04-18 16:18:20 -0400

    Minor fixes to registration

--- messages/validation/user.php
+++ messages/validation/user.php
@@ -1,12 +1,14 @@
 <?
 
 $errors = array(
-	'email' => array(
-		'email_available' => 'That email is not available.',
-	),
-	'username' => array(
-		'username_available' => 'That username is not available.',
-	),
+  'user' => array(
+    'email' => array(
+      'email_available' => 'That email is not available.',
+    ),
+    'username' => array(
+      'username_available' => 'That username is not available.',
+    ),
+  ),
 );
 
 return $errors;
