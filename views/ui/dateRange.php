5b408ef1c51d8f4969223bf3581681dc4186b481
Thiago <thiago@madeofpeople.org>
2012-04-18 17:06:03 -0400

    daterange helper, file helper, and confirmation/registration

--- views/ui/dateRange.php
+++ views/ui/dateRange.php
@@ -1,7 +1,7 @@
-<div class="ui-DateRange" data-field='reportDateRange'>
+<div class="ui-DateRange" data-autosubmit='false' data-field='reportDateRange'>
 	<?if(isset($label)):?>
 		<label><?=$label;?></label>
 	<?endif;?>
   <input type="text" name="<?=$name;?>" value="<?=$startDate;?>-<?=$endDate;?>" />
-	<img src="<?=url::base();?>lattice/lattice/resources/images/spinner.gif" width="12" height="12" alt="saving date range..." class="hidden spinner" />
+	<img src="<?=url::base();?>modules/lattice/resources/images/spinner.gif" width="12" height="12" alt="saving date range..." class="hidden spinner" />
 </div>
