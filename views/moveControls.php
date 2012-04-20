cb583083a9d6bad8c749b89a7a7215ce5ddbf3c8
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-04-20 16:28:59 -0400

    move control ux stuff

--- views/moveControls.php
+++ views/moveControls.php
@@ -1,5 +1,5 @@
 <?if( count($potentialParents) >1 ):?>
-<div class="moveWidget">
+<div class="moveWidget clearfix">
 	<h5>Move</h5>
 	<? $opts = array( 'Move this page to...' ) + $potentialParents;?>
 	<?=form::select('move', $opts ); ?>
