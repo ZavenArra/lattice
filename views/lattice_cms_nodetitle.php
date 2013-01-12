ff59939321f7cc9380b8bf969131f434a69e3d4c
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-01-12 11:05:35 -0200

    nodetitle bits related to pagemeta and page preview

--- views/lattice_cms_nodetitle.php
+++ views/lattice_cms_nodetitle.php
@@ -1,5 +1,4 @@
 <div class="objectTitle">	
-	<!-- <a class='button floatRight' href="#">Preview this Page</a> -->
 	<?
 	if($allowTitleEdit){
 		$elementArray = array( 'type'=>'text', 'name'=>'title', 'isMultiline'=>'false', 'label'=>'Title', 'class'=>'grid_7', 'tag'=>'h2', 'labelClass'=>'hidden' );
@@ -8,11 +7,14 @@
 		$elementArray = array( 'type'=>'text', 'name'=>'title', 'isMultiline'=>'false', 'label'=>'Title', 'class'=>'grid_7 inactive', 'tag'=>'h2', 'labelClass'=>'hidden' );
 		echo latticeui::buildUIElement( $elementArray, $title );
 	}	
-	if( Kohana::config('cms.enableSlugEditing') ){
-		$elementArray = array( 'type'=>'text', 'name'=>'slug', 'isMultiline'=>'false', 'label'=>'Slug', 'class'=>'grid_2 discrete', 'tag'=>'p', 'labelClass'=>'hidden' );
-		echo latticeui::buildUIElement( $elementArray, $slug );
-	}
 	?>
-		<div class="clear"></div>
-</div>
 
+	<?if( Kohana::config('cms.pageMeta') ):?>
+		<a href="#" title='Edit page metadata' class="icon meta pageMeta">Edit Page Meta</a>
+	<?endif;?>
+
+	<a class='icon preview' title='Preview this page' href="/<?=$slug;?>">Preview this Page</a>
+
+	<div class="clear"></div>
+
+</div>
