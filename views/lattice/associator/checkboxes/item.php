894659fa0f9d36f750b0dc3e047c709daaed7c5f
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-01-14 10:59:41 -0200

    updated associator/checkboxes/item.php to work with new associator setup

--- views/lattice/associator/checkboxes/item.php
+++ views/lattice/associator/checkboxes/item.php
@@ -1,4 +1,4 @@
-<label for="associatorCheckboxItem<?=$uniqueElementId;?>">
+<label for="associatorCheckboxItem_<?=$object->id;?>">
 	<?=$object->title;?>
-	<input type="checkbox" data-objectid="<?=$object->id?>" name="associatorCheckboxItem<?=$uniqueElementId;?>" <? echo $selected ? 'checked="checked"' : '';?>  />
+	<input type="checkbox" data-objectid="<?=$object->id?>" name="associatorCheckboxItem_<?=$object->id;?>" <? echo $selected ? 'checked="checked"' : '';?>  />
 </label>
