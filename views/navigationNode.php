e9a7c514c97003edce6edde5f7827aabadb37d13
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-01-19 09:17:22 -0800

    removed php truncation from navigationNode and instead doing it via css text-overflow property

--- views/navigationNode.php
+++ views/navigationNode.php
@@ -1,7 +1,10 @@
 <? $keys = array_keys($content);?>
 <li title="<?=$content['title']; ?>" class="node <?=$content['nodeType'];?> <?=$content['contentType'];?> clearFix" id="node_<?=$content['id'];?>">
-  <h5><?  echo strlen($content['title']) > 23 ? substr($content['title'], 0, 23).'...' : $content['title']; ?></h5>
-	<div class="methods">
+  <h5><?=$content['title'];?></h5>
+	
+  <?if( ( isset($content['allowTogglePublish']) && $content['allowTogglePublish']=='true' ) 
+    || ( isset( $content['allowDelete'] ) && $content['allowDelete']=='true' ) ):?>
+  <div class="methods">
 		<?if(isset($content['allowTogglePublish']) && $content['allowTogglePublish']=='true' ):?>
 		<a class="icon togglePublishedStatus <?echo $content[$keys[2]]?'published':'';?> " href="#" title="unpublish <?=$content['title'];?>">publish</a>
 		<?endif;?>
@@ -9,6 +12,7 @@
 		<a class="icon removeNode" title="delete">delete</a>
 		<?endif;?>
 	</div>
+  <?endif;?>
 </li>
 
 
