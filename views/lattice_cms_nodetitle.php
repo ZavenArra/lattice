c5773d588e79c9e2cb2b881fd674084cb2e9fa02
Deepwinter <deepwinter@winterroot.net>
2013-01-27 23:17:18 -0800

    Tweaked nodetitle styles

--- views/lattice_cms_nodetitle.php
+++ views/lattice_cms_nodetitle.php
@@ -2,14 +2,14 @@
 	<!-- <a class='button floatRight' href="#">Preview this Page</a> -->
 	<?
 	if($allowTitleEdit){
-		$elementArray = array( 'type'=>'text', 'name'=>'title', 'isMultiline'=>'false', 'label'=>'Title', 'class'=>'grid_7', 'tag'=>'h2', 'labelClass'=>'hidden' );
+		$elementArray = array( 'type'=>'text', 'name'=>'title', 'isMultiline'=>'false', 'label'=>'Title', 'class'=>'grid_7', 'tag'=>'p', 'labelClass'=>'hidden' );
 		echo latticeui::buildUIElement( $elementArray, $title );
 	}else{
-		$elementArray = array( 'type'=>'text', 'name'=>'title', 'isMultiline'=>'false', 'label'=>'Title', 'class'=>'grid_7 inactive', 'tag'=>'h2', 'labelClass'=>'hidden' );
+		$elementArray = array( 'type'=>'text', 'name'=>'title', 'isMultiline'=>'false', 'label'=>'Title', 'class'=>'grid_7 inactive', 'tag'=>'p', 'labelClass'=>'hidden' );
 		echo latticeui::buildUIElement( $elementArray, $title );
 	}	
 	if( Kohana::config('cms.enableSlugEditing') ){
-		$elementArray = array( 'type'=>'text', 'name'=>'slug', 'isMultiline'=>'false', 'label'=>'Slug', 'class'=>'grid_2 discrete', 'tag'=>'p', 'labelClass'=>'hidden' );
+		$elementArray = array( 'type'=>'text', 'name'=>'slug', 'isMultiline'=>'false', 'label'=>'Slug', 'class'=>'grid_4 discrete', 'tag'=>'p', 'labelClass'=>'hidden' );
 		echo latticeui::buildUIElement( $elementArray, $slug );
 	}
 	?>
