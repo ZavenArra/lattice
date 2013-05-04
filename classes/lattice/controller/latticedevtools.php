1368e5b7bfa32cdf123d54ad94951b18117ccdd8
quincykwende <quincykwende@gmail.com>
2013-05-05 00:54:37 +0100

    refractor latticedevtools to be able to check login and other visibility issues

--- classes/lattice/controller/latticedevtools.php
+++ classes/lattice/controller/latticedevtools.php
@@ -1,23 +1,50 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
+/**
+ * Helps dev browse through objects without passing via the CMS
+ *
+ * @package    Lattice
+ * @category   Devtools
+ * @author     quincykwende
+ * 
+ */
 
 class Lattice_Controller_Latticedevtools extends Core_Controller_Lattice
 {
+	
+	public function __construct()
+	{
+		if ( ! cms_util::check_role_access('superuser') AND PHP_SAPI != 'cli' )
+		{
+			die('Only superuser can access builder tool');
+		}
+	}
+	
 	public function action_index()
 	{
-		$this->response->body('this is the output');
+		$this->action_graph();
 	}
 
+	/**
+	 * Browser through objects
+	 *
+	 * @param   int/string id
+	 * @return  null
+	 */
 	public function action_graph($id=NULL)
 	{
-
 		$object = NULL;
-		if($id){
+		
+		if($id)
+		{
 			$object = graph::object($id);
-		} else {
+		} 
+		else 
+		{
 			$object = graph::get_lattice_root();
 		}
-
+		
+		//throw exception if object not instantiated
 		if(!is_object($object) )
 		{
 			throw new Kohana_Exception("$id is not a proper graph member");
@@ -25,29 +52,38 @@ class Lattice_Controller_Latticedevtools extends Core_Controller_Lattice
 
 		$object_type_name = $object->objecttype->objecttypename;
 
-    echo "{$object->title} &raquo; {$object->slug} <br /> | <br />";
+		echo "$object->title &raquo; $object->slug";
 
-    $lattices = array('lattice', 'artist', 'whatever'); // TODO this needs to query database for all lattices
-                                                        //  probably add an 'all_lattices' method to object or lattice model 
+		//return lattice model :: TODO this needs to return only lattices of the current object
+		$lattices = Model_Lattice::get_all_lattices();
+		
+		echo "<hr />";
+		
+		foreach($lattices as $lattice)
+		{
+			echo "<h2> Lattice: $lattice->name </h2>";
+			
+			//get lattice children
+			$children = $object->get_lattice_children($lattice->name);
 
-    foreach($lattices as $lattice){
-      echo "Lattice: $lattice <br /><br />";
+			if(!is_object($children) )
+			{
+				throw new Kohana_Exception("Database error finding children");
+			}
 
-      $children = $object->get_lattice_children($lattice);
-
-      if(!is_object($children) )
-      {
-        throw new Kohana_Exception("Database error finding children");
-      }
-
-      if(count($children) == 0){
-        // echo something about empty children ?
-      } else 
-        foreach($children as $child)
-        {
-          echo "{$child->title} &raquo; <a href=\"".url::site('latticedevtools/graph/'.$child->slug)."\">{$child->slug}</a> <br /> | <br /> ";
-        }
-    }
+			if(count($children) == 0)
+			{
+				echo "There is no child for this lattice <br />";
+			} 
+			else 
+			{
+				foreach($children as $child)
+				{
+					echo "$child->title &raquo; <a href=\"".url::site('latticedevtools/graph/'.$child->slug)."\">$child->slug</a> <br /> ";
+				}
+			}
+			echo "<hr />";
+		}
 
 		echo " --- end ---";
 
