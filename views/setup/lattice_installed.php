51cd36c239319359341bc54b934897c3d84c2513
quincykwende <quincykwende@gmail.com>
2013-08-24 21:35:10 +0100

    Moved views related to initialization to setup/

---
+++ views/setup/lattice_installed.php
@@ -0,0 +1,8 @@
+<div class="modalContainer">
+	<div class="header container_12">
+		<h3>Lattice is now installed</h3>
+	</div>
+	<div class="modal container_12">
+		<a class="button" href="<?=url::base('http');?>cms">Go to the CMS</a>
+	</div>
+</div>
---
+++ views/setup/lattice_not_installed.php
@@ -0,0 +1,8 @@
+<div class="modalContainer">
+	<div class="header container_12">
+		<h3>Lattice is Not Installed</h3>
+	</div>
+	<div class="modal container_12">
+  <a class="button" href="<?php echo url::site('setup'); ?>">Install lattice now</a>
+	</div>
+</div>
