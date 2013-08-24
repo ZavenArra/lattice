4118a2267b21620d6a463f4a9e0e8e5c5762b74f
quincykwende <quincykwende@gmail.com>
2013-08-24 20:36:55 +0100

    Moved views related to the tier/navigation to navigation/

---
+++ views/tier/navigation.php
@@ -0,0 +1,22 @@
+<div id="navigation" class="module classPath-lattice_modules_navigation_Navigation addObjectPosition-bottom">
+	<div class="breadCrumb clearFix">
+		<ul>
+		
+		</ul>
+		
+	</div>
+	<ul>
+		<?php if(Kohana::config('lattice.search_enabled')): ?>
+		<li>
+			<?php echo $search_view; ?>
+		</li>
+		<?php endif; ?>
+	</ul>
+	<div class="container">
+		<div class="panes">
+			<div class="pane">
+			</div>
+		</div>
+	</div>
+  <a href="#" class="slideToggle" >Show Navigation</a>
+</div>
---
+++ views/tier/navigation_tier.php
@@ -0,0 +1,34 @@
+<script>
+	window.addEvent('domready', function(){			
+	var dataArray = [];
+	$('search_tag').getElements('li').each(function(el,i){
+			var dataArray_new =[{name:el.get('text'),row:i}];
+			dataArray.append(dataArray_new);
+	}); var ArrSort1 = dataArray.sortBy('name').map(function(item){return item.row});
+	var ArrSort2 = dataArray.sortBy('-name').map(function(item){return item.row});
+
+	var sorter = new Fx.Sort($$('#search_tag li'),{
+	duration: 500,
+	transition: Fx.Transitions['Quartic.easeIn']
+	});
+
+	$('sort1').addEvent('click', function(event){
+	sorter.sort(ArrSort1);
+	});
+	$('sort2').addEvent('click', function(event){
+	sorter.sort(ArrSort2);
+	});
+	});
+</script>
+	<ul class="nodes" id="search_tag">
+		<?php if(Kohana::config('lattice.tier_sort_enabled')): ?>
+		Sort by: <span id="sort1" onclick="
+lattice.CMS.doAjax(this.get('rel'));" rel="desc">down</span> 
+		| <span id="sort2" rel="asc">up</span>
+		<?php endif; ?>
+	<?php foreach($nodes as $node):?>
+		<?php echo $node; ?>
+	<?php endforeach;?>
+	</ul>
+	
+	<?php echo $tier_methods_drawer; ?>
