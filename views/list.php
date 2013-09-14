b31a1c3d73895436e2dc2a3967c58d1b4948c5ae
Deepwinter <deepwinter@winterroot.net>
2013-09-13 21:33:59 -0700

    Fixed a camelCase to camel_case conversion bug

--- views/list.php
+++ views/list.php
@@ -4,13 +4,13 @@
 	<?endif;?>
 	<div class="listcontrol controls top clearfix">
 		<?foreach($addable_objects as $addable_object):?>
-		<a href="addObject/<?=$list_object_id;?>/<?=$addable_object['object_type_id'];?>" class="addItem button"><?=$addable_object['object_type_add_text'];?></a>
+		<a href="add_object/<?=$list_object_id;?>/<?=$addable_object['object_type_id'];?>" class="addItem button"><?=$addable_object['object_type_add_text'];?></a>
 		<?endforeach;?>
 	</div>	
 	<ul class="listing clearfix <?=$label;?>"><?=$items;?></ul>
 	<div class="listcontrol controls bottom clearfix">
 		<?foreach($addable_objects as $addable_object):?>
-		<a href="addObject/<?=$list_object_id;?>/<?=$addable_object['object_type_id'];?>" class="addItem button"><?=$addable_object['object_type_add_text'];?></a>	
+		<a href="add_object/<?=$list_object_id;?>/<?=$addable_object['object_type_id'];?>" class="addItem button"><?=$addable_object['object_type_add_text'];?></a>	
 		<?endforeach;?>
 	</div>
 </div>
