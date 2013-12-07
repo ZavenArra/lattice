98e7ff8727cf751c089dfa0626479473bc62582c
deepwinter@winterroot.net <deepwinter@winterroot.net>
2013-12-06 23:30:45 -0600

    Editing text

--- views/ui/search.php
+++ views/ui/search.php
@@ -1,4 +1,4 @@
 
 <div class="ui-Input search-form">
-	<input type="text" name="search_words" id="search_node"  autocomplete="off" placeholder="search active tiers for an object">
+	<input type="text" name="search_words" id="search_node"  autocomplete="off" placeholder="search active tiers">
 </div>
